5d3d3b821cfa12410e4da5e3deb9c0eeb0de73fd
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-10-30 08:36:01 +0100

    resolved a leak caused by malloc inside ft_strjoin_safe

--- minishell/preprocess_input.c
+++ minishell/preprocess_input.c
@@ -9,8 +9,11 @@ void preprocess_input(void)
     if (!expanded_input)
     {
         ft_putstr_fd("Error: failed to allocate memory\n", STDERR_FILENO);
+        free(g_data.input);        // Free g_data.input if allocated
+        free(g_data.full_input);   // Free g_data.full_input to prevent leak
         exit(EXIT_FAILURE);
     }
     free(g_data.input);
     g_data.input = expanded_input;
 }
+
