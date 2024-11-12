16df558513d5463b7cc2ba3a170ab6ebf693bffc
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-11-12 12:16:19 +0100

    prevent env to work with arguments

--- minishell/ft_env.c
+++ minishell/ft_env.c
@@ -1,16 +1,36 @@
-// ft_env.c
+// src/ft_env.c
 #include "internal_commands.h"
 
 int	ft_env(t_command *cmd)
 {
-	int	i;
+	t_token	*token;
+	int		arg_count;
+	int		i;
 
-	(void)cmd; // Unused parameter
+	token = cmd->token_list->next; // Skip the command token
+	arg_count = 0;
 
+	// Count the number of arguments provided to env
+	while (token)
+	{
+		arg_count++;
+		token = token->next;
+	}
+
+	if (arg_count > 0)
+	{
+		// env does not accept any arguments in this implementation
+		ft_putstr_fd("minishell: env: too many arguments\n", STDERR_FILENO);
+		g_data.exit_status = 1;
+		return (1);
+	}
+
+	// No arguments provided; print all environment variables
 	i = 0;
 	while (g_data.envp[i])
 	{
-		printf("%s\n", g_data.envp[i]);
+		ft_putstr_fd(g_data.envp[i], STDOUT_FILENO);
+		ft_putchar_fd('\n', STDOUT_FILENO);
 		i++;
 	}
 
