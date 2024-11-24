9d489dd7d36a8e3816a683acc4360e988d9d7902
mina-houshmand <houshmand_mina@yahoo.com>
2024-11-24 18:28:38 +0100

    main norminette

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -5,16 +5,16 @@ The isatty function is used to check if a file descriptor refers to a terminal.
 first : Makes sure that the terminal is linked to the STDIN
 */
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-    t_shell_data shell;
+	t_shell_data	shell;
+
 	(void)argc;
 	(void)argv;
-
-    if (!isatty(STDIN_FILENO))
-        exit(EXIT_FAILURE);
-    ft_bzero(&shell, sizeof(t_shell_data));
-    shell.envp = copy_envp(envp); // okey
+	if (!isatty(STDIN_FILENO))
+		exit(EXIT_FAILURE);
+	ft_bzero(&shell, sizeof(t_shell_data));
+	shell.envp = copy_envp(envp); // okey
 	if (!shell.envp)
 	{
 		ft_putstr_fd("Error: failed to allocate memory\n", STDERR_FILENO);
@@ -23,7 +23,7 @@ int main(int argc, char **argv, char **envp)
 	init_shell(&shell);
 	// setup_signal_handlers(); // Set up signal handlers
 	handle_input(&shell);
-    printf("after handle input");
+	printf("after handle input");
 	cleanup(&shell);
 	return (0);
 }
@@ -33,6 +33,4 @@ int main(int argc, char **argv, char **envp)
 - check the exit_code
 - heredoc
 - do we need to return 0 in "execute_internal_commands()"?
-*/ 
-
-
+*/
