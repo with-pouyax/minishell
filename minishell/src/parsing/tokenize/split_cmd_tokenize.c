5b1e024307940cb306a6a420131eb699a6546462
mina-houshmand <houshmand_mina@yahoo.com>
2024-11-29 12:58:52 +0100

    replace pouyas file

--- minishell/src/parsing/tokenize/split_cmd_tokenize.c
+++ minishell/src/parsing/tokenize/split_cmd_tokenize.c
@@ -1,45 +1,48 @@
 #include "../../minishell.h"
 
-int process_input_segment(t_shell_data *shell, int *i, int *cmd_index, t_command **last_cmd)
+int	process_input_segment(t_shell_data *shell, int *i, int *cmd_index, t_command **last_cmd)
 {
-	int			start;
-	char		*cmd_str;
-	t_command	*cmd;
+    int			start;
+    char		*cmd_str;
+    t_command	*cmd;
 
-	start = *i;
-	*i = extract_command_string(shell->input, *i); // Find the end index of the command
-	
-	if (*i == -1) // Check for syntax error
-	{
-		shell->error_flag = 1;
-		return (1); // Skip further processing
-	}
-	
-	cmd_str = ft_substr(shell->input, start, *i - start); // Extract the command string
-	if (!cmd_str)
-	{
-		shell->error_flag = 2;
-		return (1);
-	}
-	
-	cmd = create_command(shell, cmd_str, (*cmd_index)++); // Create a new command struct
-	if (!cmd || tokenize_command(shell, cmd))
-	{
-		free(cmd_str);
-		free(cmd);
-		shell->error_flag = 1; // Set syntax error flag
-		return (1);
-	}
-	
-	add_command_to_list(shell, last_cmd, cmd); // Add the command to the list
-	
-	if (shell->input[*i] == '|')
-		(*i)++; // Move past the pipe character
-	
-	return (0);
+    start = *i;
+    *i = extract_command_string(shell->input, *i); // Find the end index of the command
+
+    if (*i == -1) // Check for syntax error
+    {
+        shell->error_flag = 1;
+        return (1); // Skip further processing
+    }
+
+    cmd_str = ft_substr(shell->input, start, *i - start); // Extract the command string
+    if (!cmd_str)
+    {
+        shell->error_flag = 2;
+        return (1);
+    }
+
+    // Remove trailing spaces from cmd_str
+    int len = ft_strlen(cmd_str);
+    while (len > 0 && ft_isspace(cmd_str[len - 1]))
+        cmd_str[--len] = '\0';
+
+    cmd = create_command(shell, cmd_str, (*cmd_index)++); // Create a new command struct
+    if (!cmd || tokenize_command(shell, cmd))
+    {
+        free(cmd_str);
+        free(cmd);
+        shell->error_flag = 1; // Set syntax error flag
+        return (1);
+    }
+
+    add_command_to_list(shell, last_cmd, cmd); // Add the command to the list
+
+    if (shell->input[*i] == '|')
+        (*i)++; // Move past the pipe character
+
+    return (0);
 }
-
-
 void    append_end_token(t_shell_data *shell)
 {
     t_command   *cmd;
