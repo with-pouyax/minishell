956c29e82fd1efae066822648d59fa105fb09beb
mina-houshmand <houshmand_mina@yahoo.com>
2024-11-29 22:50:02 +0100

    let's call it a day

--- minishell/src/heredoc/check_delimiter.c
+++ minishell/src/heredoc/check_delimiter.c
@@ -27,24 +27,23 @@ int	get_heredoc_delimiter(char *input, int *i, t_token *heredoc_token)
 /*
 check_delimiter_quotes
 - Checks if the heredoc delimiter is quoted.
-- Removes the quotes if they exist, ensuring that special character expansions (like variables) are handled appropriately.
-
+- Removes the quotes if they exist, ensuring that special character
+expansions (like variables) are handled appropriately.
 */
-
-int check_delimiter_quotes(t_redirection *redir)
+int	check_delimiter_quotes(t_redirection *redir)
 {
-    char    *delimiter;
-    char    *unquoted_delimiter;
+	char	*delimiter;
+	char	*unquoted_delimiter;
 
-    delimiter = redir->delimiter;
-    if (delimiter[0] == '\'' || delimiter[0] == '\"')
-    {
-        unquoted_delimiter = ft_substr(delimiter, 1, ft_strlen(delimiter) - 2);
-        if (!unquoted_delimiter)
-            return (-1);
-        free(redir->delimiter);
-        redir->delimiter = unquoted_delimiter;
-        return (1);
-    }
-    return (0);
+	delimiter = redir->delimiter;
+	if (delimiter[0] == '\'' || delimiter[0] == '\"')
+	{
+		unquoted_delimiter = ft_substr(delimiter, 1, ft_strlen(delimiter) - 2);
+		if (!unquoted_delimiter)
+			return (-1);
+		free(redir->delimiter);
+		redir->delimiter = unquoted_delimiter;
+		return (1);
+	}
+	return (0);
 }
