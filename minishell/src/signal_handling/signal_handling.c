ed8af68cb1593b04fe4f598138ba9babf856d667
mina-houshmand <houshmand_mina@yahoo.com>
2024-11-29 20:59:28 +0100

    nor signal

--- minishell/src/signal_handling/signal_handling.c
+++ minishell/src/signal_handling/signal_handling.c
@@ -1,25 +1,24 @@
-// signal_handling.c
 #include "../minishell.h"
 
-volatile sig_atomic_t g_signal_status = 0;
+volatile sig_atomic_t	g_signal_status = 0;
 
-// In signal_handling.c
-void sigint_handler(int sig)
+void	sigint_handler(int sig)
 {
-    (void)sig;
-    write(1, "\n", 1);
-    rl_replace_line("", 0);
-    rl_on_new_line();
-    rl_redisplay();
-    g_signal_status = 1;
+	(void)sig;
+	write(1, "\n", 1);
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+	g_signal_status = 1;
 }
-void sigquit_handler(int sig)
+
+void	sigquit_handler(int sig)
 {
-    (void)sig;
+	(void)sig;
 }
-// In signal_handling.c
-void setup_signal_handlers(void)
+
+void	setup_signal_handlers(void)
 {
-    signal(SIGINT, sigint_handler);
-    signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, sigint_handler);
+	signal(SIGQUIT, SIG_IGN);
 }
