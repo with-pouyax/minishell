75831fc6c01b765f713db808827429e58eaf015f
mina-houshmand <houshmand_mina@yahoo.com>
2024-12-05 19:36:41 +0100

    solve cd - problem and error handling for echo

--- minishell/src/internal_cmd/internall_op/ft_cd.c
+++ minishell/src/internal_cmd/internall_op/ft_cd.c
@@ -1,5 +1,41 @@
 #include "../internal_commands.h"
 
+int handle_cd_minus(t_shell_data *shell)
+{
+    if (shell->prev_dir == NULL)
+    {
+        ft_putstr_fd("minishell: cd: OLDPWD not set\n", STDERR_FILENO);
+        shell->exit_status = 1;
+        return (1);
+    }
+
+    // Save the current directory before changing
+    char *current_dir = getcwd(NULL, 0);
+
+    // Change to the previous directory
+    if (chdir(shell->prev_dir) != 0)
+    {
+        ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+        perror(shell->prev_dir);
+        shell->exit_status = 1;
+        return (1);
+    }
+
+    // Print the previous directory after changing
+    printf("%s\n", shell->prev_dir);
+
+    // After switching, update prev_dir to the current directory
+    if (current_dir)
+    {
+        free(shell->prev_dir);  // Free any previous memory for prev_dir
+        shell->prev_dir = current_dir;  // Save the current directory as prev_dir for the next `cd -`
+    }
+
+    shell->exit_status = 0;
+    return (0);
+}
+
+
 /*
 	expanded_path = ft_strdup(home);                // Path is just "~"
 	expanded_path = ft_strjoin(home, &path[1]);     // Path starts with "~/"
@@ -62,30 +98,42 @@ int	change_to_home(t_shell_data *shell)
 	if (chdir(path) != 0)                  // Regular path
 
 */
-int	ft_cd(t_shell_data *shell, t_command *cmd)
+int ft_cd(t_shell_data *shell, t_command *cmd)
 {
-	t_token	*token;
-	char	*path;
+    t_token *token;
+    char *path;
 
 	token = cmd->token_list->next;
-	if (!token)
+    if (!token)
+    {
 		return (change_to_home(shell));
-	else
-	{
-		path = token->value;
-		if (path[0] == '~')
-			return (handle_tilde_path(shell, path));
-		else
-		{
-			if (chdir(path) != 0)
-			{
-				ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
-				perror(path);
-				shell->exit_status = 1;
-				return (1);
-			}
-		}
 	}
-	shell->exit_status = 0;
-	return (0);
+	if (token->next != NULL)
+    {
+        ft_putstr_fd("minishell: cd: too many arguments\n", STDERR_FILENO);
+        shell->exit_status = 1;
+        return (1);
+    }
+    path = token->value;
+    if (strcmp(path, "-") == 0)
+        return (handle_cd_minus(shell));
+
+    if (path[0] == '~')
+        return (handle_tilde_path(shell, path));
+    char *current_dir = getcwd(NULL, 0);  
+    if (chdir(path) != 0)
+    {
+        ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
+        perror(path);
+        shell->exit_status = 1;
+        return (1);
+    }
+    if (current_dir)
+    {
+        free(shell->prev_dir);
+        shell->prev_dir = current_dir;
+    }
+
+    shell->exit_status = 0;
+    return (0);
 }
