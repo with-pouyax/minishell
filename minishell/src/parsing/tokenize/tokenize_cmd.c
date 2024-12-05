7c43428ccd9dcdb3b55d4d2bc3c391f555304d53
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-12-05 09:46:34 +0100

    fix |    |

--- minishell/src/parsing/tokenize/tokenize_cmd.c
+++ minishell/src/parsing/tokenize/tokenize_cmd.c
@@ -28,6 +28,8 @@ t_command	*create_command(t_shell_data *shell, char *cmd_str, int index)
 
 int extract_command_string(char *input, int i)
 {
+
+
     while (input[i] && input[i] != '|') // Loop through the input string until we find a pipe
 	{
 		if (input[i] == '\'' || input[i] == '\"') // If we find a quote
@@ -35,10 +37,18 @@ int extract_command_string(char *input, int i)
 		else
 			i++;
 	}
-    if (input[i] && input[i + 1] == '|')
+    if (input[i] == '|') // If we encounter a pipe
     {
-        ft_putstr_fd("minishell: syntax error near unexpected token `|'\n", STDERR_FILENO);
-        return (-1);
+        i++; // Move past the pipe
+        while (input[i] && input[i] == ' ') // Skip spaces after the pipe
+            i++;
+        if (input[i] == '|') // If the next character is also a pipe
+        {
+            ft_putstr_fd("minishell: syntax error near unexpected token `|'\n", STDERR_FILENO);
+            return (-1);
+        }
     }
+   
+        
     return (i);
 }
