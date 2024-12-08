0c0cbc7c9f59f09500fabe668b520d67b1d7f1d6
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-12-08 13:13:29 +0100

    fixed heredoc

--- minishell/src/heredoc/check_delimiter.c
+++ minishell/src/heredoc/check_delimiter.c
@@ -1,13 +1,6 @@
 #include "../minishell.h"
 
 
-
-/*
-check_delimiter_quotes
-- Checks if the heredoc delimiter is quoted.
-- Removes the quotes if they exist, ensuring that special character
-expansions (like variables) are handled appropriately.
-*/
 int	check_delimiter_quotes(t_redirection *redir)
 {
 	char	*delimiter;
@@ -17,6 +10,17 @@ int	check_delimiter_quotes(t_redirection *redir)
 	if (delimiter[0] == '\'' || delimiter[0] == '\"')
 	{
 		unquoted_delimiter = ft_substr(delimiter, 1, ft_strlen(delimiter) - 2);
+		if (!unquoted_delimiter)
+			return (-1);
+		free(redir->delimiter);
+		redir->delimiter = unquoted_delimiter;
+		return (1);
+	}
+	if (delimiter[ft_strlen(delimiter) - 1] == '\'' || delimiter[ft_strlen(delimiter) - 1] == '\"')
+	{
+		while (delimiter[ft_strlen(delimiter) - 1] == '\'' || delimiter[ft_strlen(delimiter) - 1] == '\"')
+			delimiter[ft_strlen(delimiter) - 1] = '\0';
+		unquoted_delimiter = ft_substr(delimiter, 0, ft_strlen(delimiter));
 		if (!unquoted_delimiter)
 			return (-1);
 		free(redir->delimiter);
