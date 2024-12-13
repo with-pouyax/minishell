be3373d463007de6e8be71dc3267da4233dfa238
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-12-13 12:03:38 +0100

    fix make file

---
+++ minishell/src/internal_cmd/internall_op/ft_unset.c
@@ -0,0 +1,25 @@
+#include "../internal_commands.h"
+
+int ft_unset(t_shell_data *shell, t_command *cmd)
+{
+    t_token *token;
+
+    token = cmd->token_list->next;
+    while (token)
+    {
+        if (is_valid_identifier(token->value, 0))
+        {
+            remove_from_env(shell, token->value);
+        }
+        else
+        {
+            // ft_putstr_fd("minishell: unset: `", STDERR_FILENO);
+            // ft_putstr_fd(token->value, STDERR_FILENO);
+            //ft_putstr_fd("': not a valid identifier\n", STDERR_FILENO);
+            //shell->exit_status = 0;
+            return (0);
+        }
+        token = token->next;
+    }
+    return (0);
+}
