0f035b427120c8e02a02c5ff4fa35085c8989c52
with-pouyax <75066687+with-pouyax@users.noreply.github.com>
2024-12-17 12:18:46 +0100

    fix second ft_strdup in get_variable_value

--- minishell/src/heredoc/expand_heredoc.c
+++ minishell/src/heredoc/expand_heredoc.c
@@ -21,6 +21,10 @@ char	*get_variable_value(t_shell_data *shell, char *input, int *i, int *var_not_
 	else // If the variable is not found
 	{
 		var_value = ft_strdup(""); // Set var_value to an empty string
+		if (!var_value)
+		{
+			ft_putstr_fd("minishell: memory allocation error\n", STDERR_FILENO);
+		}
 		*var_not_found_flag = 1; // Set the var_not_found_flag to true
 	}
 	*i += var_len; // Move the index to the end of the variable name to skip it
